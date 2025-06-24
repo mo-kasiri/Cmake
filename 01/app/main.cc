633f6df327ff30d5ffc16d14dc21eff43f938095
mo-kasiri <mkgh7030@gmail.com>
2025-06-24 19:19:09 +0330

    Create multilayered cmake libraries

---
+++ 01/app/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "mylib.h"
+
+int main()
+{
+
+	std::cout << "Hello World\n";
+	print_something();
+	return 0;
+}
---
+++ 01/src/my_lib/mylib.cc
@@ -0,0 +1,6 @@
+#include "mylib.h"
+
+void print_something()
+{
+    std::cout << "something\n";
+}
